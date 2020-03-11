d3b34b62437de9b4a171cfdfde6089982f65d633
Michal Zajacik <michal.zajacik@agentfly.com>
2020-03-11 10:11:47 +0100

    pacman clean cache script, routing scripts

--- system/killDesktopApps.sh
+++ system/killDesktopApps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-deluge="/usr/bin/python2 /usr/bin/deluge-gtk"
-apps="firefox vivaldi-bin opera chromium spotify thunderbird synology-note-station qbittorrent $deluge steam"
+#deluge="/usr/bin/python2 /usr/bin/deluge-gtk"
+apps="audacious firefox vivaldi-bin opera chromium spotify thunderbird synology-note-station qbittorrent $deluge steam"
 
 function killthat(){
 	if [[ ! -z `pidof $1` ]]; then
--- system/routeLinkSys.sh
+++ system/routeLinkSys.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-sudo ip route delete default via 192.168.1.100 dev wlp0s20f0u3
-sudo ip route add 192.168.1.127 dev wlp0s20f0u3
-sudo ip route add 192.168.1.100 dev wlp0s20f0u3
+device=wlp0s20f0u5
+#sudo ip route delete default via 192.168.1.100 dev $device
+#sudo ip route add 192.168.1.127 dev $device
+#sudo ip route add 192.168.1.100 dev $device
+sudo ip route add 192.168.2.110 dev $device
+sudo ip route add 192.168.2.100 dev $device
+sudo ip route add 192.168.2.111 dev $device
+sudo ip route add 192.168.2.112 dev $device
